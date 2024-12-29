13dacf05861ca8ae337eb37cd9718b46b7d98027
roshanlimbu <topazkayle@gmail.com>
2024-12-29 15:38:38 +0545

    added flash.nvim for faster jump

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,6 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 require 'custom.autocommands.autocmds'
 require 'custom.keymaps.keymaps'
+require 'custom.plugins.harpoon'
 return {
   {
     'akinsho/toggleterm.nvim',
@@ -38,6 +35,21 @@ return {
         },
       },
       config = true,
+    },
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    vscode = true,
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
 }
